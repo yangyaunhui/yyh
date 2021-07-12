de213749b8f9d8d569ee78bc9fdcf6dfd471a7bf
@yang-yuanhui <yang07061237@163.com>
2021-07-12 10:45:15 +0800

    3.转账完成

--- com/hp/service/CustomerService.java
+++ com/hp/service/CustomerService.java
@@ -16,19 +16,19 @@ public class CustomerService {
     private Customer currentCustomer;
 
     // 1. 查, 登录 判断账号密码是否正确
-    public void  checkPwd(String cardid, String  cardPwd){
+    public void checkPwd(String cardid, String cardPwd) {
         CustomerData customerData = CustomerData.getInstance();
-        customerList  = customerData.getCustomerList();
+        customerList = customerData.getCustomerList();
         // 1. 拿到  carid中的账户名 和 cardPwd  对  list中的数据做对比
         System.out.println("cardid = " + cardid);
         System.out.println("cardPwd = " + cardPwd);
 
         for (Customer customer : customerList) {
-            if (customer.getAccount().equals(cardid) && customer.getPassword().equals(cardPwd)){
+            if (customer.getAccount().equals(cardid) && customer.getPassword().equals(cardPwd)) {
                 // 拿出来这个人
-                currentCustomer=  customer;
+                currentCustomer = customer;
                 // 账户正确
-                System.out.println("欢迎"+customer.getCname()+"顾客登录!请注意您的安全"   );
+                System.out.println("欢迎" + customer.getCname() + "顾客登录!请注意您的安全");
                 TextUtil.oneLeveOption(); //界面
                 Scanner scanner = new Scanner(System.in);
                 String option = scanner.nextLine();
@@ -40,7 +40,7 @@ public class CustomerService {
 
     private void oneOption(String option) {
 
-        switch (option){
+        switch (option) {
             case "1":
                 System.out.println("余额查询");
 
@@ -50,13 +50,11 @@ public class CustomerService {
                 goOneHome();
                 break;
             case "2":
-                System.out.println("取款");
-                withdrawMoney();
+               goGetMoneyHome();
                 goOneHome();
                 break;
             case "3":
-                System.out.println("转账");
-                Money();
+                doTuranMoney();
                 goOneHome();
                 break;
             case "4":
@@ -71,56 +69,64 @@ public class CustomerService {
 
         }
     }
+
     // 查询余额
     private void doSelectMoney() {
         double money = currentCustomer.getMoney();
-        System.out.println(" 余额是 " +money);
+        System.out.println(" 余额是 " + money);
     }
-    //取款
-    private void withdrawMoney(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入您的取款金额：");
-        int money = sc.nextInt();
-        if (money>0&&money<=currentCustomer.getMoney()){
-            currentCustomer.setMoney(currentCustomer.getMoney()-money);
-            System.out.println("取款成功");
-            System.out.println("你的当前余额为："+currentCustomer.getMoney());
-        }else {
-            System.out.println("输入错误");
+
+    //转账
+    private void doTuranMoney() {
+        System.out.println("请输入您要转账的卡号：");
+        Scanner scanner = new Scanner(System.in);
+        String otherAccount = scanner.nextLine();
+        System.out.println("请输入您要转账的金额：");
+        String otherMoney = scanner.nextLine();
+        //做计算  自己的钱 - otherMoney ,别人的钱加上 + otherMoney
+        //自己减钱
+        Double otherMoneyInt = Double.parseDouble(otherMoney);
+        double customerMoney = currentCustomer.getMoney() - otherMoneyInt;//自己被 减去转账的 钱
+        //别人加钱，根据otherAccount 本人的账户查询出 别人的余额，查询出别人的余额后，修改别人的余额
+        //因为所有人都在customerList 这个集合中
+        Customer other = null;
+        for (Customer customer : customerList) {
+            //如果 customer.getAcount 等于 otherAccount 那么这个人就被选出来了
+            if (customer.getAccount().equals(otherAccount)) {
+                other=customer;
+            }
+        }
+        double otherOneMoney = other.getMoney() + otherMoneyInt; //别人的余额
+
+        //自己和别人都跟新一下钱数
+        currentCustomer.setMoney(customerMoney);
+        other.setMoney(otherOneMoney); //注意：有问题？不能够创建对象了
+
+        for (Customer customer : customerList) {
+            System.out.println("customer = " + customer);
         }
     }
-    //转账
-    private void Money() {
-        System.out.println("请输入您要转账的卡号：");
-        Scanner account = new Scanner(System.in);
-        int money = account.nextInt();
-        Customer nuser = null;
-        boolean IsExit = false;
-    }
-
-
 
 
     //存款
-    private void saveMoney(){
+    private void saveMoney() {
         System.out.println("请输入您的存款金额：");
         Scanner input = new Scanner(System.in);
         int money = input.nextInt();
-        if (currentCustomer!=null){
-            if (money>0){
-                currentCustomer.setMoney(currentCustomer.getMoney()+money);
+        if (currentCustomer != null) {
+            if (money > 0) {
+                currentCustomer.setMoney(currentCustomer.getMoney() + money);
                 System.out.println("存款成功");
-                System.out.println("你的当前余额为："+currentCustomer.getMoney());
-            }else{
+                System.out.println("你的当前余额为：" + currentCustomer.getMoney());
+            } else {
                 System.out.println("输入错误");
             }
 
         }
     }
+
     //取卡
-
-
-    private  void goOneHome( ){
+    private void goOneHome() {
         TextUtil.oneLeveOption();
         Scanner scanner = new Scanner(System.in);
         String option = scanner.nextLine();
@@ -128,6 +134,74 @@ public class CustomerService {
         oneOption(option);  // 递归算法
     }
 
+    //取款
+    private void goGetMoneyHome() {
+        TextUtil.getMoneyUI();
+        //1.让客户输入
+        Scanner scanner = new Scanner(System.in);
+        String numIn = scanner.nextLine();
+        if (numIn.equals("1")) {
+            //那么 取款100 那么就应该 让顾客的钱减100
+            double money = currentCustomer.getMoney();
+            money = money - 100;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("2")){
+            //那么 取款200 那么就应该 让顾客的钱减200
+            double money = currentCustomer.getMoney();
+            money = money - 200;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("3")){
+            //那么 取款300 那么就应该 让顾客的钱减200
+            double money = currentCustomer.getMoney();
+            money = money - 300;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("4")){
+            //那么 取款400 那么就应该 让顾客的钱减200
+            double money = currentCustomer.getMoney();
+            money = money - 400;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("5")){
+            //那么 取款800 那么就应该 让顾客的钱减200
+            double money = currentCustomer.getMoney();
+            money = money - 800;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("6")){
+            //那么 取款1000 那么就应该 让顾客的钱减200
+            double money = currentCustomer.getMoney();
+            money = money - 1000;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("7")){
+            //那么 取款2000 那么就应该 让顾客的钱减200
+            double money = currentCustomer.getMoney();
+            money = money - 2000;
+            System.out.println("你的余额是:" + money);
+            //取完款后，更新原有存款
+            currentCustomer.setMoney(money);
+        }else if(numIn.equals("8")){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入您的取款金额：");
+            int money = sc.nextInt();
+            if (money>0&&money<=currentCustomer.getMoney()){
+                currentCustomer.setMoney(currentCustomer.getMoney()-money);
+                System.out.println("取款成功");
+                System.out.println("你的当前余额为："+currentCustomer.getMoney());
+            }else {
+                System.out.println("输入错误");
+            }
+        }
+    }
 }
 
 
--- com/hp/util/TextUtil.java
+++ com/hp/util/TextUtil.java
@@ -21,6 +21,17 @@ public class TextUtil {
         System.out.println("************");
     }
 
+    //static  方法带有static关键字的方法是静态方法，可以直接类名.方法()调用
+    public static  void getMoneyUI(){
+        System.out.println("***************************");
+        System.out.println("*******请输入你想取的数目******");
+        System.out.println("*******1.100   2.200*******");
+        System.out.println("*******3.300   4.400*******");
+        System.out.println("*******5.800   6.1000*******");
+        System.out.println("*******7.2000  8.其他********");
+        System.out.println("****************************");
+    }
+
     // 客户的 1级 选择
     public static void oneLeveOption(){
         System.out.println("*****************************************");
